b753b973da81c3421f59387722f5e3d864e85328
Gabe Herbert <gh@gabeherbert.com>
2023-08-15 17:00:15 -0700

    adds vendor dir

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'kitestring-studio/cloudflare-tools',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ba655364397af45478a23ca6a0085fbcdca8a79b',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'kitestring-studio/cloudflare-tools' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ba655364397af45478a23ca6a0085fbcdca8a79b',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
